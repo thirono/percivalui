401a2d49520965e46c10bbf2110eaa4a4cc47d40
Ulrik Kofoed Pedersen <ulrik.pedersen@diamond.ac.uk>
2015-03-05 17:07:48 +0000

    Implementing register serialization of read and write command

--- carrier/registers.py
+++ carrier/registers.py
@@ -4,13 +4,14 @@ Created on 5 Dec 2014
 @author: Ulrik Pedersen
 '''
 
-UART_ADDR_WIDTH = 16
-UART_WORD_WIDTH = 32
+from encoding import encode_multi_message, encode_message
 
 class UARTRegister(object):
     '''
     classdocs
     '''
+    UART_ADDR_WIDTH = 16
+    UART_WORD_WIDTH = 32
 
     def __init__(self, name, start_addr, words_per_entry, entries, readback_addr):
         '''
@@ -18,28 +19,34 @@ class UARTRegister(object):
         '''
         self._name = name
         self._start_addr = start_addr
+        if start_addr.bit_length() > self.UART_ADDR_WIDTH:
+            raise ValueError("start_addr value 0x%H is greater than 16 bits"%start_addr)
         self._words_per_entry = words_per_entry
         self._entries = entries
         self._readback_addr = readback_addr
         
         # Setup a container of data words
-        # Ensure it's a deep copy by [:]
+        # Ensure each entry list is an individual instance by using xrange()
         self._data_words = [[0x0000]*self._words_per_entry for i in xrange(self._entries)]
         
     def get_read_cmdmsg(self):
-        pass
+        read_cmdmsg = encode_message(self._readback_addr, 0x00000000)
+        return read_cmdmsg
     
     def get_write_cmdmsg(self):
-        pass
+        # Flatten the 2D matrix of datawords into one continuous list
+        contiguous_data_words = [word for entry in self._data_words for word in entry]
+        write_cmdmsg = encode_multi_message(self._start_addr, contiguous_data_words)
+        return write_cmdmsg
     
     def set_data_word(self, entry, word_index, word):
-        if word.bit_length() > 32:
+        if word.bit_length() > self.UART_WORD_WIDTH:
             raise ValueError("Word 0x%H is larger than 32 bits"%word)
         self._data_words[entry][word_index] = word
     
     def set_data_entry(self, entry, words):
         for word in words:
-            if word.bit_length() > 32:
+            if word.bit_length() > self.UART_WORD_WIDTH:
                 raise ValueError("Word 0x%H is larger than 32 bits"%word)
         self._data_words[entry] = words
     
