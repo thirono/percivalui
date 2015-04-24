684eee27b9bd47d31f5747986f61e73f4f0bfc41
Ulrik Kofoed Pedersen <ulrik.pedersen@diamond.ac.uk>
2015-04-24 15:43:51 +0100

    Adding tests to ensure encoding returns arrays of bytes rather than strings or anything else

--- carrier/test_encoding.py
+++ carrier/test_encoding.py
@@ -14,11 +14,13 @@ class TestEncodeMessage(unittest.TestCase):
     def test_single(self):
         '''Testing the encode_message() function with very basic inputs.'''
         result = encoding.encode_message(0x0005, 0x01020304)
+        self.assertIsInstance(result, bytes, "Must return a byte array rather than %s"%type(result))
         self.assertEqual(result, b'\x00\x05\x01\x02\x03\x04', str([result]))
         
     def test_multi(self):
         '''Testing the encode_multi_message() function with very basic input'''
         result = encoding.encode_multi_message(0x000A, [0x01020304, 0x05060708])
+        self.assertIsInstance(result, bytes, "Must return a byte array rather than %s"%type(result))
         self.assertEqual(result, b'\x00\x0A\x01\x02\x03\x04\x00\x0B\x05\x06\x07\x08', str([result]))
         
     def test_invalid_data_word(self):
@@ -35,6 +37,7 @@ class TestDecodeMessage(unittest.TestCase):
     def test_basic(self):
         '''Basic sanity check of the decode_message() function'''
         result = encoding.decode_message(b'\x00\x05\x01\x02\x03\x04')
+        self.assertIsInstance(result, list, "Must be a 2D list rather than %s"%type(result))
         self.assertGreater(len(result), 0, "Result list contain no items")
         addr, word = result[0]
         self.assertEqual(addr, 0x0005, "Address not decoded properly: %X"%addr)
@@ -43,6 +46,7 @@ class TestDecodeMessage(unittest.TestCase):
     def test_multi_response(self):
         '''Test decode_message() with a series of addr,word responses...'''
         result = encoding.decode_message(b'\xAA\xBB\x01\x02\x03\x04\xCC\xDD\x05\x06\x07\x08\xEE\xFF\x09\x10\x11\x12')
+        self.assertIsInstance(result, list, "Must be a 2D list rather than %s"%type(result))
         self.assertEqual(len(result), 3, "Not the expected number of responses in result (%d)"%len(result))
         addr, word = result[0]
         self.assertEqual(addr, 0xAABB, "Address[%d] not decoded properly: %X"%(0, addr))
@@ -58,6 +62,7 @@ class TestDecodeMessage(unittest.TestCase):
         '''Test the decode_message() function when a message string is parsed with some additional rubbish bytes.
            It should throw away the additional bytes and return an otherwise sensible response'''
         result = encoding.decode_message(b'\x00\x05\x01\x02\x03\x04\xEE\xAA')
+        self.assertIsInstance(result, list, "Must be a 2D list rather than %s"%type(result))
         self.assertGreater(len(result), 0, "Result list contain no items")
         addr, word = result[0]
         self.assertEqual(addr, 0x0005, "Address not decoded properly: %X"%addr)
