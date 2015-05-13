09a85192b5e74ec909ca22a24135e81fe8e835c6
Ulrik Kofoed Pedersen <ulrik.pedersen@diamond.ac.uk>
2015-05-13 13:03:37 +0100

    Improved TxRx class with context manager
    Added example which uses the context manager in the sandbox.

--- percival/carrier/txrx.py
+++ percival/carrier/txrx.py
@@ -4,34 +4,75 @@ Created on 4 Dec 2014
 @author: Ulrik Pedersen
 '''
 from __future__ import unicode_literals, absolute_import
+import logging
 
 import socket
+from contextlib import contextmanager 
 
 class TxRx(object):
     '''
     classdocs
     '''
 
-    def __init__(self, fpga_addr):
+    def __init__(self, fpga_addr, port = 10001, timeout = 2.0):
         '''
         Constructor
         '''
+        self.log = logging.getLogger(".".join([__name__, self.__class__.__name__]))
+        
+        self._fpga_addr = (fpga_addr, port)
+        
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.settimeout(1.0)
-        self.sock.connect((fpga_addr, 10001))
+        self.sock.settimeout(timeout)
+        self.log.debug("connecting to FPGA: %s", str(self._fpga_addr))
+        self.sock.connect(self._fpga_addr)
+    
+    @property
+    def fpga_addr(self):
+        return self._fpga_addr
+    
+    @property
+    def timeout(self):
+        return self.sock.gettimeout()
+    @timeout.setter
+    def timeout(self, value):
+        self.sock.settimeout(value)
     
     def tx_msg(self, msg):
         self.sock.sendall(msg)
     
-    def rx_msg(self, expected_bytes):
+    def rx_msg(self, expected_bytes = None):
         msg = ''
-        while len(msg) < expected_bytes:
-            chunk = self.sock.recv(expected_bytes-len(msg))
+        block_read_bytes = expected_bytes
+        expected_resp_len = expected_bytes
+        
+        if expected_bytes == None: 
+            expected_resp_len = 1
+            block_read_bytes = 1024
+        
+        while len(msg) < expected_resp_len:
+            if expected_bytes:
+                block_read_bytes = expected_bytes-len(msg)
+            chunk = self.sock.recv(block_read_bytes)
             if chunk == '':
                 raise RuntimeError("socket connection broken")
             msg = msg + chunk
         return msg
 
+    def send_recv(self, msg, expected_bytes = None):
+        self.tx_msg(msg)
+        resp = self.rx_msg(expected_bytes)
+        return resp
     
+    def clean(self):
+        self.sock.shutdown(socket.SHUT_RDWR)
+        self.sock.close()
+        
+@contextmanager
+def TxRxContext(*args, **kwargs):
+    trx = TxRx(*args, **kwargs)
+    yield trx
+    trx.clean()
     
-    
+       
+            
---
+++ sandbox/complexsocket.py
@@ -0,0 +1,35 @@
+'''
+Created on 13 May 2015
+
+@author: up45
+'''
+from __future__ import print_function
+import logging
+logging.basicConfig()
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+from percival.carrier.txrx import TxRx, TxRxContext
+from percival.carrier.encoding import (encode_message, encode_multi_message, decode_message)
+
+board_ip_address = "percival2.diamond.ac.uk"
+
+def main():
+    log.debug("complexsocket...")
+    
+    with TxRxContext(board_ip_address) as trx:
+        
+        #msg = encode_message(0x0144, 0x00000000)
+        msg = encode_message(0x0144, 0x00000000)
+        
+        log.debug("as string: %s",str([msg]))
+        resp = trx.send_recv(msg)
+    
+    log.debug([resp])
+    
+    resp = decode_message(resp)
+    log.debug(resp)
+    
+if __name__ == '__main__':
+    main()
+    
