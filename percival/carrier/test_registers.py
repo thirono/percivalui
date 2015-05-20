02c9b0cd3df98a1f66f8bfb996db9f4f310f2acd
Ulrik Kofoed Pedersen <ulrik.pedersen@diamond.ac.uk>
2015-05-20 13:14:50 +0100

    Fix broken registers test case

--- percival/carrier/test_registers.py
+++ percival/carrier/test_registers.py
@@ -25,15 +25,16 @@ class TestUARTRegister(unittest.TestCase):
         self.assertEqual(msg.message, b'\x01\x44\x00\x00\x00\x00', "Readback msg not correct: %s"%str([msg]))
         
     def test_write_msg(self):
-        self.command_reg.settings.device_index = 1
-        self.assertEqual(self.command_reg.settings.device_index, 1)
+        self.command_reg.settings.parse_map([0,0,0])
+        self.assertEqual(self.command_reg.settings.device_index, 0)
+        self.assertEqual(self.command_reg.settings.device_cmd, 0)
         msg = self.command_reg.get_write_cmdmsg()
         self.assertTrue(type(msg), list)
         self.assertIsInstance(msg[0], txrx.TxMessage)
         self.assertEqual(len(msg), 3)
         expected_msg = [bytes('\x00\xEC\x00\x00\x00\x00', encoding='latin-1'),
                         bytes('\x00\xED\x00\x00\x00\x00', encoding='latin-1'),
-                        bytes('\x00\xEF\x00\x00\x00\x00', encoding='latin-1')]
+                        bytes('\x00\xEE\x00\x00\x00\x00', encoding='latin-1')]
         for i in range(3):
             self.assertEqual(msg[i].message, expected_msg[i], msg[i].message)
         
