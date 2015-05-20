bb004d77584d63fa9c87dc262434d25ec108448e
Ulrik Kofoed Pedersen <ulrik.pedersen@diamond.ac.uk>
2015-05-20 15:37:24 +0100

    sandbox readdevice: adding actual transmit/receive command/response

--- sandbox/readdevice.py
+++ sandbox/readdevice.py
@@ -5,6 +5,8 @@ Created on 19 May 2015
 '''
 from __future__ import print_function
 
+import time, binascii
+
 from percival.log import log
 
 from percival.carrier.registers import UARTRegister
@@ -16,37 +18,52 @@ board_ip_address = "percival2.diamond.ac.uk"
 def main():
     log.debug("read device...")
     
-    #with TxRxContext(board_ip_address) as trx:
+    with TxRxContext(board_ip_address) as trx:
     
-    cmd = UARTRegister(0x00EC)  # Command register
-    cmd.settings.parse_map([0,0,0]) # initialise all registers to 0
+        cmd = UARTRegister(0x00EC)  # Command register
+        cmd.settings.parse_map([0,0,0]) # initialise all registers to 0
+        
+        # First generate and send a no-op system command
+        cmd.settings._mem_map["system_cmd"].value = 0 # no-op system command
+        cmd.settings._mem_map["system_cmd_data"].value = 0  # not used
+        no_op_cmd_msg = cmd.get_write_cmdmsg()[2]
+        log.info("System no-op command: %s", str(no_op_cmd_msg))
+        response = trx.send_recv_message(no_op_cmd_msg)
+        decoded_response = decode_message(response)
+        log.info("    response: 0x%04X: 0x%08X", decoded_response[0][0], decoded_response[0][1])
+       
+        cmd.settings._mem_map["system_cmd"].value = 2 # disable global monitoring
+        disable_global_mon_cmd_msg = cmd.get_write_cmdmsg()[2]
+        log.info("System disable global monitoring command: %s", str(disable_global_mon_cmd_msg))
+        response = trx.send_recv_message(disable_global_mon_cmd_msg)
+        decoded_response = decode_message(response)
+        log.info("    response: 0x%04X: 0x%08X", decoded_response[0][0], decoded_response[0][1])
+        
+        cmd.settings._mem_map["device_cmd"].value = 0 # device no-op
+        cmd.settings._mem_map["device_type"].value = 1 # device monitoring
+        cmd.settings._mem_map["device_index"].value = 2 # T sensor...
+        device_no_op_cmd_msg = cmd.get_write_cmdmsg()[0]
+        log.info("Device no-op command: %s", str(device_no_op_cmd_msg))
+        response = trx.send_recv_message(device_no_op_cmd_msg)
+        decoded_response = decode_message(response)
+        log.info("    response: 0x%04X: 0x%08X", decoded_response[0][0], decoded_response[0][1])
     
-    # First generate and send a no-op system command
-    cmd.settings._mem_map["system_cmd"].value = 0 # no-op system command
-    cmd.settings._mem_map["system_cmd_data"].value = 0  # not used
-    no_op_cmd_msg = cmd.get_write_cmdmsg()[2]
-    log.info("System no-op command: %s", str(no_op_cmd_msg))
-    
-   
-    cmd.settings._mem_map["system_cmd"].value = 2 # disable global monitoring
-    disable_global_mon_cmd_msg = cmd.get_write_cmdmsg()[2]
-    log.info("System disable global monitoring command: %s", str(disable_global_mon_cmd_msg))
-    
-    cmd.settings._mem_map["device_cmd"].value = 0 # device no-op
-    cmd.settings._mem_map["device_type"].value = 1 # device monitoring
-    cmd.settings._mem_map["device_index"].value = 2 # T sensor...
-    device_no_op_cmd_msg = cmd.get_write_cmdmsg()[0]
-    log.info("Device no-op command: %s", str(device_no_op_cmd_msg))
-
-    cmd.settings._mem_map["device_cmd"].value = 5 # device set and get
-    log.debug("cmd map: %s", cmd.settings.generate_map())
-    log.debug("       : %s", cmd.settings._mem_map)
-    device_set_and_get_cmd_msg = cmd.get_write_cmdmsg()[0]
-    log.info("Device get and set command: %s", str(device_set_and_get_cmd_msg))
-    
-    read_echo_cmd_msg = cmd.get_read_cmdmsg()  # READ ECHO WORD
-    log.info("READ ECHO WORD command: %s", str(read_echo_cmd_msg))
-    
+        cmd.settings._mem_map["device_cmd"].value = 5 # device set and get
+        log.debug("cmd map: %s", cmd.settings.generate_map())
+        log.debug("       : %s", cmd.settings._mem_map)
+        device_set_and_get_cmd_msg = cmd.get_write_cmdmsg()[0]
+        log.info("Device get and set command: %s", str(device_set_and_get_cmd_msg))
+        response = trx.send_recv_message(device_set_and_get_cmd_msg)
+        decoded_response = decode_message(response)
+        log.info("    response: 0x%04X: 0x%08X", decoded_response[0][0], decoded_response[0][1])
+        
+        while True:
+            read_echo_cmd_msg = cmd.get_read_cmdmsg()  # READ ECHO WORD
+            log.info("READ ECHO WORD command: %s", str(read_echo_cmd_msg))
+            response = trx.send_recv_message(read_echo_cmd_msg)
+            decoded_response = decode_message(response)
+            log.info("    response: 0x%04X: 0x%08X", decoded_response[0][0], decoded_response[0][1])
+            time.sleep(0.5)
     
     
     
