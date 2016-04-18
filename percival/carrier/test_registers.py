ae4665e01688e8f2ffbc9d44df7203e3337dd493
Ulrik Kofoed Pedersen <ulrik.pedersen@diamond.ac.uk>
2016-04-18 09:55:03 +0100

    Updated unittest to latest 2016.02.16 firmware version

--- percival/carrier/test_registers.py
+++ percival/carrier/test_registers.py
@@ -14,17 +14,14 @@ logging.getLogger(__name__).setLevel(logging.DEBUG)
 class TestUARTRegister(unittest.TestCase):
 
     def setUp(self):
-        self.command_reg = registers.UARTRegister( 0x00EC ) # Command Register
+        self.command_reg = registers.UARTRegister( 0x0170 ) # Command Register
         cl = ".".join([__name__,str( self.__class__)])
         self.log = logging.getLogger(cl)
 
-    def test_read_msg(self):
-        '''Check the correctness of the generated readback command/msg from get_read_cmdmsg()'''
-        msg = self.command_reg.get_read_cmdmsg()
-        self.assertIsInstance(msg, txrx.TxMessage)
-        self.assertIs(msg.expected_bytes, 3 * 6)
-        self.assertEqual(msg.message, b'\x01\x44\x00\x00\x00\x00', "Readback msg not correct: %s"%str([msg]))
-        
+    def test_invalid_command_readback_msg(self):
+        '''Check that a Readback message from get_read_cmdmsg() throws an exception as Command has no readback shortcut'''
+        with self.assertRaises( TypeError) as cm: self.command_reg.get_read_cmdmsg( )
+
     def test_write_msg(self):
         self.command_reg.settings.parse_map([0,0,0])
         self.assertEqual(self.command_reg.settings.device_index, 0)
@@ -33,9 +30,9 @@ class TestUARTRegister(unittest.TestCase):
         self.assertTrue(type(msg), list)
         self.assertIsInstance(msg[0], txrx.TxMessage)
         self.assertEqual(len(msg), 3)
-        expected_msg = [bytes('\x00\xEC\x00\x00\x00\x00', encoding='latin-1'),
-                        bytes('\x00\xED\x00\x00\x00\x00', encoding='latin-1'),
-                        bytes('\x00\xEE\x00\x00\x00\x00', encoding='latin-1')]
+        expected_msg = [bytes('\x01\x70\x00\x00\x00\x00', encoding='latin-1'),
+                        bytes('\x01\x71\x00\x00\x00\x00', encoding='latin-1'),
+                        bytes('\x01\x72\x00\x00\x00\x00', encoding='latin-1')]
         for i in range(3):
             self.assertEqual(msg[i].message, expected_msg[i], msg[i].message)
         
