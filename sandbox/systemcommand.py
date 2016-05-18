82c3b4429d183e46a3c3e3ff4d58dfa3091127d4
Alan Greer <ajg@osllx10.observatorysciences.co.uk>
2016-05-18 17:17:20 +0100

    Updated systemcommand script to make use of the system class file.

--- sandbox/systemcommand.py
+++ sandbox/systemcommand.py
@@ -11,8 +11,8 @@ import argparse
 from percival.log import log
 
 from percival.carrier import const
-from percival.carrier.registers import UARTRegister
 from percival.carrier.txrx import TxRxContext
+from percival.carrier.system import SystemCommand
 
 board_ip_address = os.getenv("PERCIVAL_CARRIER_IP")
 
@@ -22,28 +22,14 @@ def options():
     args = parser.parse_args()
     return args
 
-
 def main():
     args = options()
     log.info (args)
 
     with TxRxContext(board_ip_address) as trx:
-        reg_command = UARTRegister(const.COMMAND)
-        reg_command.initialize_map([0,0,0])
 
-        log.debug("Sending command: %s", const.SystemCmd.no_operation)
-        reg_command.fields.system_cmd = const.SystemCmd.no_operation.value
-        cmd_msg = reg_command.get_write_cmd_msg(True)[2]
-        response = trx.send_recv_message(cmd_msg)
-        # Log the message response
-        log.debug("Response: %s", response)
-
-        log.debug("Sending command: %s", const.SystemCmd[args.action])
-        reg_command.fields.system_cmd = const.SystemCmd[args.action].value
-        cmd_msg = reg_command.get_write_cmd_msg(True)[2]
-        response = trx.send_recv_message(cmd_msg)
-        # Log the message response
-        log.debug("Response: %s", response)
+        sys_cmd = SystemCommand(trx)
+        sys_cmd.send_command(const.SystemCmd[args.action])
 
 
 if __name__ == '__main__':
