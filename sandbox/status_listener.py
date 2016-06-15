d0fa23eaa6d44034d985ea26a603888ee9384364
Alan Greer <ajg@osllx10.observatorysciences.co.uk>
2016-06-15 17:26:54 +0100

    Created simple script to subscribe to and log any published status items from the percival application.

---
+++ sandbox/status_listener.py
@@ -0,0 +1,40 @@
+'''
+Created on 15 June 2016
+
+@author: Alan Greer
+'''
+from __future__ import print_function
+from future.utils import raise_with_traceback
+
+import argparse
+
+from percival.log import log
+
+import os
+from percival.detector.ipc_channel import IpcChannel
+
+board_ip_address = os.getenv("PERCIVAL_CARRIER_IP")
+
+
+def options():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--endpoint", default="tcp://127.0.0.1:8889", help="Subscribe to this endpoint")
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = options()
+    log.info(args)
+
+    log.debug("Subscribing to channel %s", args.endpoint)
+    channel = IpcChannel(IpcChannel.CHANNEL_TYPE_SUB)
+    channel.connect(args.endpoint)
+    channel.subscribe("")
+
+    while True:
+        raw_msg = channel.recv()
+        log.debug(raw_msg)
+
+if __name__ == '__main__':
+    main()
