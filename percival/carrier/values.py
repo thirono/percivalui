f191a2ed9f6d9b63a010e211eb4fe22c01a03b6b
Alan Greer <ajg@osllx10.observatorysciences.co.uk>
2016-06-15 11:08:32 +0100

    Added a values file with a simple BoardValues class, that currently simply reads out the values.

---
+++ percival/carrier/values.py
@@ -0,0 +1,37 @@
+'''
+Created on 10 June 2016
+
+@author: gnx91527
+'''
+from __future__ import print_function
+
+import os, time
+
+import logging
+from percival.log import log
+
+from percival.carrier.registers import UARTRegister, BoardValueRegisters, generate_register_maps
+
+class BoardValues:
+    def __init__(self, txrx, board):
+        self.log = logging.getLogger(self.__class__.__name__)
+        self._txrx = txrx
+        self._board = board
+        self._value_block = BoardValueRegisters[board]
+        self._reg_monitoring_values = UARTRegister(self._value_block)
+        self._cmd_msg = self._reg_monitoring_values.get_read_cmd_msg()
+
+
+    def read_values(self):
+        """Read all carrier monitor channels with one READ VALUES shortcut command
+
+        Parse the resuling [(address, data), (address, data)...] array of tuples into a list of
+        :class:`percival.carrier.register.ReadValueMap` objects.
+
+        :returns: list of :class:`percival.carrier.register.ReadValueMap` objects.
+        :rtype: list
+        """
+        response = self._txrx.send_recv_message(self._cmd_msg)
+        read_maps = generate_register_maps(response)
+        #result = dict(zip(self._channel_data.keys(), read_maps))
+        return read_maps
