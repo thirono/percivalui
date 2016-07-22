3dac7f5bc827f72e82fa446a197e9589e1d986ef
Alan Greer <ajg@osllx10.observatorysciences.co.uk>
2016-07-22 14:54:57 +0100

    Revert "Updated unit tests to match latest firmware version (and therefore pass!)"
    This reverts commit d37e4b301a6ce5abf1459b61cdde54a099edfb5c.

--- percival/carrier/test_channels.py
+++ percival/carrier/test_channels.py
@@ -25,7 +25,7 @@ class TestChannels(unittest.TestCase):
         self.settings = [(0x00, 0x00), (0x00, 0x00), (0x00, 0x00), (0x00, 0x00)]
 
     def TestControlChannel(self):
-        self.txrx.send_recv_message = MagicMock(return_value=[(0x01B2, 0x00000019)])
+        self.txrx.send_recv_message = MagicMock(return_value=[(0x0139, 0x00000019)])
         self.channel_ini.Component_family_ID = const.DeviceFamily.AD5629
         self.channel_ini._channel_number = 5
         self.channel_ini.UART_address = 19
@@ -33,21 +33,21 @@ class TestChannels(unittest.TestCase):
         ctrlChannel = ControlChannel(self.txrx, self.channel_ini, self.settings)
         # Verify the txrx mock had send_rcv_message called
         self.txrx.send_recv_message.assert_called_with(
-            TxMessage(bytes("\x01\x70\x20\x00\x00\x05", encoding="latin-1"), expect_eom=True))
+            TxMessage(bytes("\x00\xF8\x20\x00\x00\x05", encoding="latin-1"), expect_eom=True))
 
         # Send a no-op command
         self.txrx.send_recv_message.reset_mock()
         ctrlChannel.cmd_no_operation()
         # Verify the txrx mock had send_rcv_message called
         self.txrx.send_recv_message.assert_called_with(
-            TxMessage(bytes("\x01\x70\x00\x00\x00\x05", encoding="latin-1"), expect_eom=True))
+            TxMessage(bytes("\x00\xF8\x00\x00\x00\x05", encoding="latin-1"), expect_eom=True))
 
         # Send a set and get value
         self.txrx.send_recv_message.reset_mock()
         ctrlChannel.cmd_set_and_get_value()
         # Verify the txrx mock had send_rcv_message called
         self.txrx.send_recv_message.assert_called_with(
-            TxMessage(bytes("\x01\x70\x50\x00\x00\x05", encoding="latin-1"), expect_eom=True))
+            TxMessage(bytes("\x00\xF8\x50\x00\x00\x05", encoding="latin-1"), expect_eom=True))
 
 
         # Send a read echo word command
@@ -55,7 +55,7 @@ class TestChannels(unittest.TestCase):
         ctrlChannel.read_echo_word()
         # Verify the txrx mock had send_rcv_message called
         self.txrx.send_recv_message.assert_called_with(
-            TxMessage(bytes("\x01\xCA\x00\x00\x00\x00", encoding="latin-1"), expect_eom=False))
+            TxMessage(bytes("\x01\x51\x00\x00\x00\x00", encoding="latin-1"), expect_eom=False))
 
         # Set the value to 10
         self.txrx.send_recv_message.reset_mock()
@@ -78,18 +78,18 @@ class TestChannels(unittest.TestCase):
         # Verify the txrx mock had send_rcv_message called
         calls = self.txrx.send_recv_message.mock_calls
         self.assertEqual(calls[0], call(
-            TxMessage(bytes("\x01\x70\x00\x00\x00\x05", encoding="latin-1"), expect_eom=True)))
+            TxMessage(bytes("\x00\xF8\x00\x00\x00\x05", encoding="latin-1"), expect_eom=True)))
         self.assertEqual(calls[1], call(
             TxMessage(bytes("\x00\x16\x00\x00\x00\x19", encoding="latin-1"), expect_eom=True)))
         self.assertEqual(calls[2], call(
-            TxMessage(bytes("\x01\x70\x00\x00\x00\x05", encoding="latin-1"), expect_eom=True)))
+            TxMessage(bytes("\x00\xF8\x00\x00\x00\x05", encoding="latin-1"), expect_eom=True)))
         self.assertEqual(calls[3], call(
-            TxMessage(bytes("\x01\x70\x50\x00\x00\x05", encoding="latin-1"), expect_eom=True)))
+            TxMessage(bytes("\x00\xF8\x50\x00\x00\x05", encoding="latin-1"), expect_eom=True)))
         self.assertEqual(calls[4], call(
-            TxMessage(bytes("\x01\xCA\x00\x00\x00\x00", encoding="latin-1"), expect_eom=False)))
+            TxMessage(bytes("\x01\x51\x00\x00\x00\x00", encoding="latin-1"), expect_eom=False)))
 
     def TestAD5242ControlChannel(self):
-        self.txrx.send_recv_message = MagicMock(return_value=[(0x01B2, 0x00000019)])
+        self.txrx.send_recv_message = MagicMock(return_value=[(0x0139, 0x00000019)])
         self.channel_ini.Component_family_ID = const.DeviceFamily.AD5242
         self.channel_ini._channel_number = 5
         self.channel_ini.UART_address = 19
@@ -101,7 +101,7 @@ class TestChannels(unittest.TestCase):
             TxMessage(bytes("\x00\x16\x00\x01\x00\x0A", encoding="latin-1"), expect_eom=True))
 
     def TestMonitoringChannel(self):
-        self.txrx.send_recv_message = MagicMock(return_value=[(0x01B2, 0x01000023)])
+        self.txrx.send_recv_message = MagicMock(return_value=[(0x0139, 0x01000023)])
         self.channel_ini.Component_family_ID = const.DeviceFamily.MAX31730
         self.channel_ini._channel_number = 18
         self.channel_ini.UART_address = 139
@@ -115,20 +115,20 @@ class TestChannels(unittest.TestCase):
         # Verify the txrx mock had send_rcv_message called
         calls = self.txrx.send_recv_message.mock_calls
         self.assertEqual(calls[0], call(
-            TxMessage(bytes("\x01\xCA\x00\x00\x00\x00", encoding="latin-1"), expect_eom=False)))
+            TxMessage(bytes("\x01\x51\x00\x00\x00\x00", encoding="latin-1"), expect_eom=False)))
         self.assertEqual(calls[1], call(
-            TxMessage(bytes("\x01\x70\x00\x80\x00\x12", encoding="latin-1"), expect_eom=True)))
+            TxMessage(bytes("\x00\xF8\x00\x80\x00\x12", encoding="latin-1"), expect_eom=True)))
         self.assertEqual(calls[2], call(
-            TxMessage(bytes("\x01\x70\x50\x80\x00\x12", encoding="latin-1"), expect_eom=True)))
+            TxMessage(bytes("\x00\xF8\x50\x80\x00\x12", encoding="latin-1"), expect_eom=True)))
         self.assertEqual(calls[3], call(
-            TxMessage(bytes("\x01\xCA\x00\x00\x00\x00", encoding="latin-1"), expect_eom=False)))
+            TxMessage(bytes("\x01\x51\x00\x00\x00\x00", encoding="latin-1"), expect_eom=False)))
 
         # Setup the mock with different sample numbers
         self.txrx.send_recv_message = MagicMock() #return_value=[(0x0139, 0x01000023), (0x0139, 0x02000023)])
-        self.txrx.send_recv_message.side_effect = [[(0x01B2, 0x01000012)],
-                                                   [(0x01B2, 0xABBABAC1)],
-                                                   [(0x01B2, 0xABBABAC1)],
-                                                   [(0x01B2, 0x02000013)]]
+        self.txrx.send_recv_message.side_effect = [[(0x0139, 0x01000012)],
+                                                   [(0x0139, 0xABBABAC1)],
+                                                   [(0x0139, 0xABBABAC1)],
+                                                   [(0x0139, 0x02000013)]]
         mntrChannel = MonitoringChannel(self.txrx, self.channel_ini, self.settings)
         self.txrx.send_recv_message.reset_mock()
         value = mntrChannel.get_value()
--- percival/carrier/test_values.py
+++ percival/carrier/test_values.py
@@ -21,26 +21,26 @@ class TestBoardValuesClass(unittest.TestCase):
         self.value = BoardValues(self.txrx, const.BoardTypes.carrier)
         self.value.read_values()
         self.txrx.send_recv_message.assert_called_with(
-            TxMessage(bytes("\x01\xC6\x00\x00\x00\x00", encoding="latin-1"), num_response_msg=19, expect_eom=False))
+            TxMessage(bytes("\x01\x4D\x00\x00\x00\x00", encoding="latin-1"), num_response_msg=19, expect_eom=False))
 
         self.txrx.send_recv_message.reset_mock()
         # Test reading values from periphery left board
         self.value = BoardValues(self.txrx, const.BoardTypes.left)
         self.value.read_values()
         self.txrx.send_recv_message.assert_called_with(
-            TxMessage(bytes("\x01\xC4\x00\x00\x00\x00", encoding="latin-1"), num_response_msg=16, expect_eom=False))
+            TxMessage(bytes("\x01\x4B\x00\x00\x00\x00", encoding="latin-1"), num_response_msg=1, expect_eom=False))
 
         self.txrx.send_recv_message.reset_mock()
         # Test reading values from periphery bottom board
         self.value = BoardValues(self.txrx, const.BoardTypes.bottom)
         self.value.read_values()
         self.txrx.send_recv_message.assert_called_with(
-            TxMessage(bytes("\x01\xC5\x00\x00\x00\x00", encoding="latin-1"), num_response_msg=2, expect_eom=False))
+            TxMessage(bytes("\x01\x4C\x00\x00\x00\x00", encoding="latin-1"), num_response_msg=1, expect_eom=False))
 
         self.txrx.send_recv_message.reset_mock()
         # Test reading values from plugin board
         self.value = BoardValues(self.txrx, const.BoardTypes.plugin)
         self.value.read_values()
         self.txrx.send_recv_message.assert_called_with(
-            TxMessage(bytes("\x01\xC7\x00\x00\x00\x00", encoding="latin-1"), num_response_msg=2, expect_eom=False))
+            TxMessage(bytes("\x01\x4E\x00\x00\x00\x00", encoding="latin-1"), num_response_msg=1, expect_eom=False))
 
