0feb664c51330f155c361d9abf9782e2f816057b
Ulrik Kofoed Pedersen <ulrik.pedersen@diamond.ac.uk>
2017-01-27 12:55:18 +0000

    Adding unittest to load ini files.
    These tests are fairly crude and will error rather than fail if the UART addresses in Channel parameters.ini are not consistent.

--- percival/carrier/test_settings.py
+++ percival/carrier/test_settings.py
@@ -9,13 +9,34 @@ import unittest, sys, logging
 from mock import MagicMock
 from builtins import bytes
 
-class TestSettings(unittest.TestCase):
+from carrier.configuration import BoardParameters
+from detector.detector import PercivalParameters
+from percival.carrier import const
+from percival.carrier.settings import BoardSettings
+
+
+class TestBoardSettings(unittest.TestCase):
 
     def setUp(self):
         # Perform any setup here
         self.log = logging.getLogger(self.__class__.__name__)
         self.log.setLevel(logging.DEBUG)
+        self.txrx = MagicMock()
+        self.parameters = PercivalParameters()
+        self.parameters.load_ini()
 
-    def TestControlChannel(self):
-        # Stub
-        self.log.debug("Test Stub")
+    def TestInitialiseBoardLeft(self):
+        bs = BoardSettings(self.txrx, const.BoardTypes.left)
+        bs.initialise_board(self.parameters)
+
+    def TestInitialiseBoardBottom(self):
+        bs = BoardSettings(self.txrx, const.BoardTypes.bottom)
+        bs.initialise_board(self.parameters)
+
+    def TestInitialiseBoardCarrier(self):
+        bs = BoardSettings(self.txrx, const.BoardTypes.carrier)
+        bs.initialise_board(self.parameters)
+
+    def TestInitialiseBoardPlugin(self):
+        bs = BoardSettings(self.txrx, const.BoardTypes.plugin)
+        bs.initialise_board(self.parameters)
