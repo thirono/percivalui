21d3a288345656cc5861e14caaf0383a67a92acc
Ulrik Kofoed Pedersen <ulrik.pedersen@diamond.ac.uk>
2017-01-28 23:45:10 +0000

    Updating simulator to 2017.01.24 address space

--- sandbox/simplesimulator.py
+++ sandbox/simplesimulator.py
@@ -34,59 +34,35 @@ class ShortcutRegister(object):
 
 
 def main():
-    registers = np.zeros(0x0152, dtype=np.uint32)
-    eoms = range(0x0000, 0x00FB)
-    shortcuts = {0x013A: ShortcutRegister(0x0000, 1 * 1),
-                 0x013B: ShortcutRegister(0x0001, 1 * 4),
-                 0x013C: ShortcutRegister(0x0005, 1 * 4),
-                 0x013D: ShortcutRegister(0x0009, 1 * 1),
-                 0x013E: ShortcutRegister(0x000A, 1 * 4),
-                 0x013F: ShortcutRegister(0x000E, 1 * 4),
-                 0x0140: ShortcutRegister(0x0012, 1 * 1),
-                 0x0141: ShortcutRegister(0x0013, 14 * 4),
-                 0x0142: ShortcutRegister(0x004B, 19 * 4),
-                 0x0143: ShortcutRegister(0x0097, 1 * 1),
-                 0x0144: ShortcutRegister(0x0098, 1 * 4),
-                 0x0145: ShortcutRegister(0x009C, 1 * 4),
-                 0x0146: ShortcutRegister(0x00A0, 1 * 32),
-                 0x0147: ShortcutRegister(0x00C0, 1 * 8),
-                 0x0148: ShortcutRegister(0x00C8, 1 * 8),
-                 0x0149: ShortcutRegister(0x00D0, 1 * 8),
-                 0x014A: ShortcutRegister(0x00D8, 1 * 32),
-                 0x014B: ShortcutRegister(0x00F8, 1 * 3),
-                 0x014C: ShortcutRegister(0x00FB, 1 * 1),
-                 0x014D: ShortcutRegister(0x00FD, 19 * 1),
-                 0x014E: ShortcutRegister(0x0110, 1 * 1),
-                 0x014F: ShortcutRegister(0x0111, 1 * 8),
-                 0x0150: ShortcutRegister(0x0119, 1 * 32),
-                 0x0151: ShortcutRegister(0x0139, 1 * 1)}
+    registers = np.zeros(0x0407, dtype=np.uint32)
+    eoms = range(0x0000, 0x03FF)
+    shortcuts = {0x03EF: ShortcutRegister(0x0000, 1 * 1),
+                 0x03F0: ShortcutRegister(0x0001, 1 * 4),
+                 0x03F1: ShortcutRegister(0x0005, 1 * 4),
+                 0x03F2: ShortcutRegister(0x0009, 1 * 1),
+                 0x03F3: ShortcutRegister(0x000A, 52 * 4),
+                 0x03F4: ShortcutRegister(0x00DA, 84 * 4),
+                 0x03F5: ShortcutRegister(0x022A, 1 * 1),
+                 0x03F6: ShortcutRegister(0x022B, 14 * 4),
+                 0x03F7: ShortcutRegister(0x0263, 19 * 4),
+                 0x03F8: ShortcutRegister(0x02AF, 1 * 1),
+                 0x03F9: ShortcutRegister(0x02B0, 1 * 4),
+                 0x03FA: ShortcutRegister(0x02B4, 1 * 4),
+                 0x03FB: ShortcutRegister(0x02B8, 1 * 32),
+                 0x03FC: ShortcutRegister(0x02D8, 1 * 8),
+                 0x03FD: ShortcutRegister(0x02E0, 1 * 18),
+                 0x03FE: ShortcutRegister(0x02F2, 1 * 8),
+                 0x03FF: ShortcutRegister(0x02FA, 1 * 64),
+                 0x0000: ShortcutRegister(0x033A, 1 * 3),  # command
 
-#    registers = np.zeros(0x01CA, dtype=np.uint32)
-#    eoms = range(0x0000, 0x01B2)
-#    shortcuts = {0x01B3: ShortcutRegister(0x0000, 1*1),
-#                 0x01B4: ShortcutRegister(0x0001, 16*4),
-#                 0x01B5: ShortcutRegister(0x0041, 16*4),
-#                 0x01B6: ShortcutRegister(0x0081, 1*1),
-#                 0x01B7: ShortcutRegister(0x0082, 2*4),
-#                 0x01B8: ShortcutRegister(0x008A, 2*4),
-#                 0x01B9: ShortcutRegister(0x0092, 1*1),
-#                 0x01BA: ShortcutRegister(0x0093, 14*4),
-#                 0x01BB: ShortcutRegister(0x00CB, 19*4),
-#                 0x01BC: ShortcutRegister(0x0117, 1*1),
-#                 0x01BD: ShortcutRegister(0x0118, 2*4),
-#                 0x01BE: ShortcutRegister(0x0120, 2*4),
-#                 0x01BF: ShortcutRegister(0x0128, 1*32),
-#                 0x01C0: ShortcutRegister(0x0148, 1*8),
-#                 0x01C1: ShortcutRegister(0x0150, 1*8),
-#                 0x01C2: ShortcutRegister(0x0158, 1*8),
-#                 0x01C3: ShortcutRegister(0x0160, 1*16),
-#                 0x01C4: ShortcutRegister(0x0173, 16*1),
-#                 0x01C5: ShortcutRegister(0x0183, 2*1),
-#                 0x01C6: ShortcutRegister(0x0185, 19*1),
-#                 0x01C7: ShortcutRegister(0x0198, 2*1),
-#                 0x01C8: ShortcutRegister(0x019A, 1*8),
-#                 0x01C9: ShortcutRegister(0x01A2, 1*16),
-#                 0x01CA: ShortcutRegister(0x01B2, 1*1)}
+                 0x0400: ShortcutRegister(0x033D, 1 * 1),  # read left
+                 0x0401: ShortcutRegister(0x033E, 84 * 1),  # read bottom
+                 0x0402: ShortcutRegister(0x0392, 19 * 1),  # read carrier
+                 0x0403: ShortcutRegister(0x03A5, 1 * 1),   # read plugin
+                 0x0404: ShortcutRegister(0x03A6, 1 * 8),   # read status
+                 0x0405: ShortcutRegister(0x03AE, 1 * 64),  # read buffer
+                 0x0406: ShortcutRegister(0x03EE, 1 * 1)}
+
 
     server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_sock.settimeout(None)
@@ -124,6 +100,7 @@ def main():
                     client_sock.send(msg)
                 else:
                     if a in eoms:
+                        log.debug("EOM required")
                         # We need to send FFFFABBABAC1 as an end of message
                         client_sock.send(END_OF_MESSAGE)
                     else:
@@ -133,26 +110,26 @@ def main():
                 # Implementation of some expected results
                 # If set value called for VCH device then the next read echo
                 # is happy if it sees the same value
-                if a == 0x0016 or\
-                    a == 0x001A or\
-                    a == 0x001E or\
-                    a == 0x0022 or\
-                    a == 0x0026 or\
-                    a == 0x002A or\
-                    a == 0x002E or\
-                    a == 0x0032:
-                    registers[0x0139] = w
+                if a == 0x022E or\
+                    a == 0x0232 or\
+                    a == 0x0236 or\
+                    a == 0x023A or\
+                    a == 0x023E or\
+                    a == 0x0242 or\
+                    a == 0x0246 or\
+                    a == 0x0250:
+                    registers[0x03EE] = w
 
                 # Implementation of some expected results
                 # If set value called for DPOT device then the next read echo
                 # is happy if it sees the same value without the flag for power
-                if a == 0x0036 or\
-                    a == 0x003A or\
-                    a == 0x003E or\
-                    a == 0x0042 or\
-                    a == 0x0046 or\
-                    a == 0x004A:
-                    registers[0x0139] = w & 0x0FFFF
+                if a == 0x0254 or\
+                    a == 0x0258 or\
+                    a == 0x025C or\
+                    a == 0x0260 or\
+                    a == 0x0264 or\
+                    a == 0x0268:
+                    registers[0x03EE] = w & 0x0FFFF
 
             chunk = client_sock.recv(block_read_bytes)
 
