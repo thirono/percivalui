928ca47c05a814aa177ede0dff32d44e172394aa
Ulrik Kofoed Pedersen <ulrik.pedersen@diamond.ac.uk>
2017-01-28 13:34:28 +0000

    Improved systemcommand.py script robustness:
    - Now checks if a system command exist instead of failing with an exception.
    - Using PercivalParameters list to load ini files and get Carrier IP address from this class.
    - Printing a few user-friendly messages to indicate what is happening.
    - Changing the default Carrier IP address in percival.ini from localhost to 192.168.0.2

--- sandbox/systemcommand.py
+++ sandbox/systemcommand.py
@@ -5,7 +5,7 @@ Created on 17 May 2016
 '''
 from __future__ import print_function
 
-import os
+import sys
 import argparse
 
 from percival.log import log
@@ -13,13 +13,17 @@ from percival.log import log
 from percival.carrier import const
 from percival.carrier.txrx import TxRxContext
 from percival.carrier.system import SystemCommand
+from percival.detector.detector import PercivalParameters
 
-board_ip_address = os.getenv("PERCIVAL_CARRIER_IP")
+system_commands = "\n\t".join([name for name, tmp in const.SystemCmd.__members__.items()])
 
 
 def options():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-a", "--action", action="store", default="no_operation", help="System command to send")
+    desc = """Send a System Command to the Percival Carrier Board
+    """
+    parser = argparse.ArgumentParser(description=desc)
+    action_help = "System command to send. Valid commands are: %s" % system_commands
+    parser.add_argument("-a", "--action", action="store", default="no_operation", help=action_help)
     args = parser.parse_args()
     return args
 
@@ -28,10 +32,23 @@ def main():
     args = options()
     log.info(args)
 
-    with TxRxContext(board_ip_address) as trx:
+    try:
+        system_command = const.SystemCmd[args.action.lower()]
+    except KeyError:
+        log.error("Invalid command \'%s\' supplied to --action", args.action)
+        print("Invalid command: \'%s\'" % args.action)
+        print("Valid commands are: \n\t%s" % system_commands)
+        sys.exit(-1)
 
+    pcvl_params = PercivalParameters()
+    pcvl_params.load_ini()
+    log.info("Connecting to Carrier IP: %s", pcvl_params.carrier_ip)
+
+    with TxRxContext(pcvl_params.carrier_ip) as trx:
+        log.debug("Connected to %s", pcvl_params.carrier_ip)
+        print("Sending System Command: %s", system_command.name)
         sys_cmd = SystemCommand(trx)
-        sys_cmd.send_command(const.SystemCmd[args.action])
+        sys_cmd.send_command(system_command)
 
 
 if __name__ == '__main__':
