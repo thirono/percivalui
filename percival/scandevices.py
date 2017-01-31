b9a8d2abe293f72a5714815936cab38eb1064ebc
Ulrik Kofoed Pedersen <ulrik.pedersen@diamond.ac.uk>
2017-01-31 12:02:49 +0000

    scandevices adding summary status report
    Logging deferred to a file and user messages printed on stdout.

--- percival/scandevices.py
+++ percival/scandevices.py
@@ -11,7 +11,7 @@ import numpy as np
 import h5py
 from collections import OrderedDict
 
-from percival.log import log
+from percival.log import get_exclusive_file_logger
 from percival.carrier import const
 from percival.carrier.registers import UARTRegister, generate_register_maps
 from percival.carrier.settings import BoardSettings
@@ -19,7 +19,7 @@ from percival.carrier.txrx import TxRxContext, hexify
 from percival.carrier.channels import ControlChannel
 from percival.detector.detector import PercivalParameters
 
-board_ip_address = os.getenv("PERCIVAL_CARRIER_IP")
+log = get_exclusive_file_logger('percival-scan-devices.log')
 
 
 class ReadMonitors(object):
@@ -68,10 +68,9 @@ class ReadMonitors(object):
         """
         Get all the recorded channel data as a (channel) dict of (field) dicts with numpy arrays:
 
-            { "channel one": { "field_one": numpy.array([...])},
-                               "field_two": numpy.array([...])},
-              "channel two": { "field_one": numpy.array([...])},
-                               "field_two": numpy.array([...])}}
+            { "channel one": { "field_one": numpy.array([1,2,3]), "field_two": numpy.array([4,5,6])},
+              "channel two": { "field_one": numpy.array([1,1,1]), "field_two": numpy.array([0,0,0])} }
+
 
         :return: dictionary of dictionaries of numpy arrays
         """
@@ -111,9 +110,12 @@ def store_monitor_data(args, data_dict):
 
 def options():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-r", "--range", default="0,100,20", help="Scan range in integers formatted like this: start,stop,step")
+    parser.add_argument("-r", "--range", default="0,100,20",
+                        help="Scan range in integers formatted like this: start,stop,step")
     parser.add_argument("-o", "--output", action='store', help="Output HDF5 filename")
     parser.add_argument("-p", "--period", action='store', type=float, default=1.0, help="Control the loop period time")
+    parser.add_argument("-s", "--status", action='store_true', default=False,
+                        help="Print status report at the end of scan")
     parser.add_argument("channel", action='store', help="Control Channel to scan")
     args = parser.parse_args()
 
@@ -125,23 +127,21 @@ def main():
     args = options()
     log.info(args)
 
-    with TxRxContext(board_ip_address) as trx:
-        percival_params = PercivalParameters()
-        percival_params.load_ini()
+    percival_params = PercivalParameters()
+    percival_params.load_ini()
+    log.debug("INI parameters: %s", percival_params)
 
-        log.debug("INI parameters: %s", percival_params)
-
+    with TxRxContext(percival_params.carrier_ip) as trx:
         channel_ini = percival_params.control_channel_by_name(args.channel)
         log.info("ini: %s", channel_ini)
 
         board_type = const.BoardTypes(channel_ini.Board_type)
         bs = BoardSettings(trx, board_type)
-        #bs.initialise_board(percival_params)
         bs.readback_control_settings()
         cc_settings = bs.device_control_settings(channel_ini.UART_address)
-        log.info("Control Channel #2 settings from board: %s", hexify(cc_settings))
+        log.info("Control Channel settings from board: %s", hexify(cc_settings))
 
-        log.debug("Creating control channel")
+        log.debug("Creating ControlChannel object: %s, %s", channel_ini, cc_settings)
         cc = ControlChannel(trx, channel_ini, cc_settings)
 
         readmons = [ReadMonitors(trx, const.READ_VALUES_CARRIER, percival_params, const.BoardTypes.carrier),
@@ -150,31 +150,23 @@ def main():
                     ReadMonitors(trx, const.READ_VALUES_PLUGIN, percival_params, const.BoardTypes.plugin)]
 
         tstamp = time.time()
-        for new_value in range(*args.range):
-            log.info("Writing DAC channel 2 value = %d", new_value)
+        scan_range = args.range
+        scan_range[1] += scan_range[2]  # Increment by one to include the final scan step as requested by HW/FW devs
+        print(" | ".join(["Demand", "ECHO value", "Sample Number", "I2C Error", "Notice"]))
+        for new_value in range(*scan_range):
+            log.info("Writing Control Channel \'%s\' value = %d", args.channel, new_value)
             echo_result = cc.set_value(new_value, timeout=1.0)
             log.info("ECHO: %s", echo_result)
+            comment = ""
             if echo_result.read_value != new_value:
                 log.warning("  Echo result does not match demanded value (%d != %d)", echo_result.read_value, new_value)
-            dt = time.time() - tstamp
-            if args.period - dt > 0.0:
-                time.sleep(args.period - dt)
-                log.debug("sleeping: %f sec", args.period - dt)
-            tstamp = time.time()
-            adcs = {}
-            [adcs.update(rm.read_monitors_devices()) for rm in readmons]
-            log.info("Read carrier monitoring channels: %s", adcs.keys())
+                comment += "WARNING readback does not match demand! "
+            if echo_result.i2c_communication_error:
+                log.warning("  I2C communication error")
+                comment += "I2C comms error!"
+            print(" | ".join([str(field) for field in [new_value, echo_result.read_value, echo_result.sample_number,
+                                                       echo_result.i2c_communication_error, comment]]))
 
-        # check if we need to execute one more iteration
-        new_value = 0
-        if (args.range[2] > 0 and new_value < args.range[1]) or (args.range[2] < 0 and new_value > args.range[1]):
-            new_value = args.range[1]
-            # Execute one last time to include the maximum value
-            log.info("Writing DAC channel 2 value = %d", new_value)
-            echo_result = cc.set_value(new_value, timeout=1.0)
-            log.info("ECHO: %s", echo_result)
-            if echo_result.read_value != new_value:
-                log.warning("  Echo result does not match demanded value (%d != %d)", echo_result.read_value, new_value)
             dt = time.time() - tstamp
             if args.period - dt > 0.0:
                 time.sleep(args.period - dt)
@@ -187,10 +179,29 @@ def main():
         #log.info(readmon.channel_data)
 
     if args.output:
+        print("Writing recorded data to file: ", args.output)
         mon_data = {}
         [mon_data.update(rm.channel_data) for rm in readmons]
         store_monitor_data(args, mon_data)
 
+    if args.status:
+        print("\n  Error status report. Any monitoring channel with any error flag is reported here. "
+              "Each status parameter is reported with a count of the number of samples where the status flag was set.")
+        print(" | ".join(["Channel", 'i2c_communication_error',
+                        'above_extreme_high_threshold', 'above_high_threshold',
+                        'below_extreme_low_threshold', 'below_low_threshold',
+                        'safety_exception_detected']))
+        for ch in mon_data:
+            status_list = [mon_data[ch]['i2c_communication_error'],
+                          mon_data[ch]['above_extreme_high_threshold'],
+                          mon_data[ch]['above_high_threshold'],
+                          mon_data[ch]['below_extreme_low_threshold'],
+                          mon_data[ch]['below_low_threshold'],
+                          mon_data[ch]['safety_exception_detected']]
+
+            errors = [sta.any() for sta in status_list]
+            if True in errors:
+                print(" | ".join([ch] + [str(len(sta.nonzero()[0])) for sta in status_list]))
 
 if __name__ == '__main__':
     main()
