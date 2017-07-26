3640296b5ba1607a0260f0e0ea5ca3804b2a9e88
Alan Greer <alan.greer@diamond.ac.uk>
2017-07-26 09:33:03 +0100

    Created two sandbox scripts that generate the default sensor ini files.

---
+++ sandbox/GenerateConfigurationIni.py
@@ -0,0 +1,37 @@
+'''
+Created on 24 July 2017
+
+@author: Alan Greer
+'''
+from __future__ import print_function
+from builtins import range  # pylint: disable=W0622
+from percival.log import log
+
+
+def main():
+    log.info("Generating file SensorCalibration.ini in config dir...")
+
+    try:
+        with open("config/SensorConfiguration.ini", "w") as c_file:
+            c_file.write("[General]\n")
+            c_file.write("Cols_<H1>= 704\n")
+            c_file.write("Cols_<H0>= 704\n")
+            c_file.write("Cols_<G>= 32\n")
+            c_file.write("\n")
+            c_file.write("[H1]\n")
+            for index in range(704):
+                c_file.write("Col{}=0\n".format(index))
+            c_file.write("\n")
+            c_file.write("[H0]\n")
+            for index in range(704):
+                c_file.write("Col{}=0\n".format(index))
+            c_file.write("\n")
+            c_file.write("[G]\n")
+            for index in range(32):
+                c_file.write("Col{}=0\n".format(index))
+            c_file.write("\n")
+    except:
+        log.info("File generation failed...")
+
+if __name__ == '__main__':
+    main()
