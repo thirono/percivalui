7db88ec07c9455577b09e7efcdb632f5744377ec
Alan Greer <alan.greer@diamond.ac.uk>
2017-08-01 09:30:04 +0100

    Added comment regarding the mkdir log handler.

--- percival/mkdir_handler.py
+++ percival/mkdir_handler.py
@@ -1,8 +1,16 @@
+"""
+Created on 1 August 2017
+
+@author: Alan Greer
+"""
 import os
 from logging.handlers import RotatingFileHandler
 
 
 class MkDirRotatingFileHandler(RotatingFileHandler):
+    """
+    Subclass of RotatingFileHandler that attempts to create any directories required by the log file.
+    """
     def __init__(self, filename, mode='a', maxBytes=0, backupCount=0, encoding=None, delay=0):
         dir = os.path.dirname(filename)
         if not os.path.exists(dir):
